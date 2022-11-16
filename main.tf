19fc6e9a4c0f96d89ffbce66649f4daa78aff9e6
Naveen-NSC <105866558+Naveen-NSC@users.noreply.github.com>
2022-11-16 12:25:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 data "aws_ami" "ubuntu" {
     most_recent = true
 filter {
-        name   = "app-server"
+        name   = "name"
         values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
     }
 filter {
