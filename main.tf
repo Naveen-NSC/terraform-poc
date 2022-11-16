c157d57563ccca7af77488e9a2f2531af1036a1a
Naveen-NSC <105866558+Naveen-NSC@users.noreply.github.com>
2022-11-16 12:53:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,19 @@ resource "aws_instance" "app_server" {
   ami           = "ami-062df10d14676e201"
   instance_type = "t2.micro"
   key_name      = "naveen"
+  vpc_security_group_ids = [
+    "sg-0f6b406cfcc00712f"
+  ]
+  user_data = <<EOF
+#!/bin/bash
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+usermod -G wheel ec2-user  
+chmod 666 /var/run/docker.sock
+usermod -aG docker ec2-user  
+EOF
+  
 tags = {
     Name = var.ec2_name
   }
